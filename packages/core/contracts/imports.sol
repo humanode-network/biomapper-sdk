dd8240d8777be2a0aae281a59d0d3c983041aa91
Tony C <tonycdot@yahoo.com>
2024-02-19 01:37:43 +0700

    Move core

--- packages/core/contracts/imports.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.20;
-
-// Interfaces imports for correct ABI generation.
-import {ICheckUniqueness} from "../interfaces/ICheckUniqueness.sol";
-import {IBiomapperLogRead} from "../interfaces/IBiomapperLogRead.sol";
