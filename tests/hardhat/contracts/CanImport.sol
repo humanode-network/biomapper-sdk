8fd4344ebbae7a5bdcd4652715023c41e2e5899f
MOZGIII <mike-n@narod.ru>
2024-02-20 14:34:24 -0300

    Add hardhat test

---
+++ tests/hardhat/contracts/CanImport.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.24;
+
+import {ICheckUniqueness} from "@biomapper-sdk/core/ICheckUniqueness.sol";
+import {IBiomapperLogRead} from "@biomapper-sdk/core/IBiomapperLogRead.sol";
+import {BiomapperLogLib} from "@biomapper-sdk/libraries/BiomapperLogLib.sol";
