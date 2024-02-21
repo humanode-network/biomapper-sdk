6816709e4dd0f4127f8b67b7d2d395e8916774e9
MOZGIII <mike-n@narod.ru>
2024-02-21 16:13:20 -0300

    Correct the code doc for the interfaces

--- core/IBiomapperLogRead.sol
+++ core/IBiomapperLogRead.sol
@@ -1,154 +1,127 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-/**
- * @dev Interface of `BiomapperLog` contract.
- *
- * Example of loop through all generations:
- *
- * ```solidity
- * uint256 generation = BiomapperLog.generationsHead();
- *
- * while (true) {
- *     // Do something with a generation.
- *
- *     generation = BiomapperLog
- *         .generationsListItem({ptr: generation})
- *         .prevPtr;
- *
- *     if (generation == 0) {
- *         // It was the oldest generation.
- *         break;
- *     }
- * }
- * ```
- *
- * Generations could also be searched from oldest to current.
- * The loop with such ordering:
- *
- * ```solidity
- * uint256 generation = BiomapperLog.generationsTail();
- *
- * while (true) {
- *     // Do something with a generation.
- *
- *     generation = BiomapperLog
- *         .generationsListItem({ptr: generation})
- *         .nextPtr;
- *
- *     if (generation == 0) {
- *         // It was the current generation.
- *         break;
- *     }
- * }
- * ```
- */
+/// @dev Interface for the `BiomapperLog` contract.
+/// @notice View historical biomapping state for a given account,
+/// and overall generation changes.
+///
+/// #### Examples
+///
+/// A loop through all generations.
+///
+/// ```solidity
+/// uint256 generation = BiomapperLog.generationsHead();
+///
+/// while (true) {
+/// 	// Do something with a generation.
+///
+///     generation = BiomapperLog
+///         .generationsListItem({ptr: generation})
+///         .prevPtr;
+///
+///     if (generation == 0) {
+///         // It was the oldest generation.
+///       break;
+///     }
+/// }
+/// ```
+///
+/// Generations could also be searched from oldest to current.
+///
+/// ```solidity
+/// uint256 generation = BiomapperLog.generationsTail();
+///
+/// while (true) {
+///     // Do something with a generation.
+///
+///     generation = BiomapperLog
+///         .generationsListItem({ptr: generation})
+///         .nextPtr;
+///
+///     if (generation == 0) {
+///         // It was the current generation.
+///         break;
+///     }
+/// }
+/// ```
 interface IBiomapperLogRead {
-    /**
-     * @dev Structure representing a biomapper generation.
-     *
-     * Pointer of {Generation} is a number of block that contains a 'generation change' transaction.
-     */
+    /// @notice Structure representing a biomapper generation.
+    ///
+    /// Pointer of {Generation} is a number of block that contains a 'generation change' transaction.
     struct Generation {
-        /**
-         * @dev A salted hash of some deployment data.
-         * Do not use.
-         */
+        /// @notice A salted hash of some deployment data.
+        /// Deprecated, do not use.
         bytes32 generation; // Deprecated field
-        /**
-         * @dev Block number of the previous generation.
-         */
+        /// @notice Block number of the previous generation.
         uint256 prevPtr;
-        /**
-         * @dev Block number of the next generation.
-         */
-
+        /// @notice Block number of the next generation.
         uint256 nextPtr;
     }
 
-    /**
-     * @dev Structure representing a biomapping.
-     *
-     * Pointer of {Biomapping} is a number of block that contains a 'prove uniqueness' transaction.
-     */
+    /// @notice Structure representing a biomapping.
+    ///
+    /// Pointer of {Biomapping} is a number of block that contains a 'prove uniqueness' transaction.
     struct Biomapping {
-        /**
-         * @dev Pointer to the generation that has this biomapping.
-         */
+        /// @notice Pointer to the generation that has this biomapping.
         uint256 generationPtr;
-        /**
-         * @dev Block number of the previous biomapping.
-         */
+        /// @notice Block number of the previous biomapping.
         uint256 prevPtr;
-        /**
-         * @dev Block number of the next biomapping.
-         */
+        /// @notice Block number of the next biomapping.
         uint256 nextPtr;
     }
 
-    /**
-     * @dev Returns the most recent biomapping pointer for a given account address.
-     * @param account The address of the requested account.
-     * @return The block number in which the most recent biomap occurred, or 0 if the account was never biomapped.
-     */
+    /// @notice Returns the most recent biomapping pointer for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the most recent biomap occurred, or 0 if the account was never biomapped.
     function biomappingsHead(address account) external view returns (uint256);
 
-    /**
-     * @dev Returns the oldest biomapping pointer for a given account address.
-     * @param account The address of the requested account.
-     * @return The block number in which the oldest biomap occurred, or 0 if the account was never biomapped.
-     */
+    /// @notice Returns the oldest biomapping pointer for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the oldest biomap occurred, or 0 if the account was never biomapped.
     function biomappingsTail(address account) external view returns (uint256);
 
-    /**
-     * @dev Returns the biomapping struct for a given biomapping pointer and account address.
-     * @param account The address of the requested account.
-     * @param ptr The biomapping pointer of the requested biomapping.
-     * @return The {Biomapping} structure.
-     *
-     * See {generationsListItem}.
-     */
+    /// @notice Returns the biomapping struct for a given biomapping pointer and account address.
+    /// @param account The address of the requested account.
+    /// @param ptr The biomapping pointer of the requested biomapping.
+    /// @return The {Biomapping} structure.
+    ///
+    /// See {generationsListItem}.
     function biomappingsListItem(
         address account,
         uint256 ptr
     ) external view returns (Biomapping memory);
 
-    /**
-     * @dev Returns the biomapping pointer for a given account address and generation pointer.
-     * @param account The address of the requested account.
-     * @param generationPtr The pointer of the requested generation.
-     * @return The block number in which the requested user was biomapped in the requested generation, or 0 if there was no biomapping.
-     */
+    /// @notice Returns the biomapping pointer for a given account address and generation pointer.
+    /// @param account The address of the requested account.
+    /// @param generationPtr The pointer of the requested generation.
+    /// @return The block number in which the requested user was biomapped in the requested generation, or 0 if there was no biomapping.
     function generationBiomapping(
         address account,
         uint256 generationPtr
     ) external view returns (uint256);
 
-    /**
-     * @dev Returns the block number in which the current generation began.
-     * @return The block number in which the current generation began.
-     */
+    /// @notice Returns the block number in which the current generation began.
+    /// @return The block number in which the current generation began.
     function generationsHead() external view returns (uint256);
 
-    /**
-     * @dev Returns the block number in which the oldest generation began.
-     * @return The block number in which the oldest generation began.
-     */
+    /// @notice Returns the block number in which the oldest generation began.
+    /// @return The block number in which the oldest generation began.
     function generationsTail() external view returns (uint256);
 
-    /**
-     * @dev Returns the generation struct for a given generation pointer.
-     * @param ptr The pointer of the requested generation.
-     * @return The {Generation} structure.
-     *
-     * Example of obtaining previous generation pointer:
-     *
-     * ```solidity
-     * previousGeneration = BiomapperLog
-     *     .generationsListItem({ptr: generation})
-     *     .prevPtr;
-     * ```
-     */
+    /// @notice Returns the generation struct for a given generation pointer.
+    ///
+    /// #### Examples
+    ///
+    /// Obtaining previous generation pointer:
+    ///
+    /// ```solidity
+    /// previousGeneration = BiomapperLog
+    ///     .generationsListItem({ptr: generation})
+    ///     .prevPtr;
+    /// ```
+    ///
+    /// @param ptr The pointer of the requested generation.
+    /// @return The {Generation} structure.
     function generationsListItem(
         uint256 ptr
     ) external view returns (Generation memory);
--- core/ICheckUniqueness.sol
+++ core/ICheckUniqueness.sol
@@ -1,14 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.20;
 
-/**
- * @dev Interface of `Biomapper` contract.
- */
+/// @dev Interface of `Biomapper` contract.
+/// @notice View the current biomapping state of a given account.
 interface ICheckUniqueness {
-    /**
-     * @dev Determines the uniqueness status of a given address in the current biomapper generation.
-     * @param queriedAddress The address to check for uniqueness.
-     * @return A boolean value indicating whether the address is biomapped (true) or not (false).
-     */
+    /// @notice Determines the uniqueness status of a given address in the current biomapper generation.
+    /// @param queriedAddress The address to check for uniqueness.
+    /// @return A boolean value indicating whether the address is biomapped (true) or not (false).
     function isUnique(address queriedAddress) external view returns (bool);
 }
