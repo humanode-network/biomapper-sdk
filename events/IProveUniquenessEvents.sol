2cd5db40631ffe51ab76420a1661b60e21bcc158
Tony C <tonycdot@yahoo.com>
2024-02-28 00:01:28 +0700

    Add event interfaces

---
+++ events/IProveUniquenessEvents.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+/// @dev Interface for the `Biomapper` contract.
+/// @notice Interface for events related to proving uniqueness.
+/// ## Exhaustiveness
+/// This contract may emit other events not covered by this interface.
+interface IProveUniquenessEvents {
+    /// @notice Emitted when uniqueness is successfully proven and a new biomapping is added.
+    /// @param account The address of the account for which uniqueness is proven.
+    /// @param biotoken The biotoken associated with the account.
+    event NewBiomapping(address account, bytes32 biotoken);
+}
