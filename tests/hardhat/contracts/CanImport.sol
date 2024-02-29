5367369c8f47257ebad36fd01129c966bd78278c
MOZGIII <mike-n@narod.ru>
2024-02-29 19:47:29 -0300

    Add events to the import test

--- tests/hardhat/contracts/CanImport.sol
+++ tests/hardhat/contracts/CanImport.sol
@@ -4,3 +4,5 @@ pragma solidity ^0.8.24;
 import {ICheckUniqueness} from "@biomapper-sdk/core/ICheckUniqueness.sol";
 import {IBiomapperLogRead} from "@biomapper-sdk/core/IBiomapperLogRead.sol";
 import {BiomapperLogLib} from "@biomapper-sdk/libraries/BiomapperLogLib.sol";
+import {IGenerationChangeEvents} from "@biomapper-sdk/events/IGenerationChangeEvents.sol";
+import {IProveUniquenessEvents} from "@biomapper-sdk/events/IProveUniquenessEvents.sol";
