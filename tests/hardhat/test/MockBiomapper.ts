4b58412cdca7cbd0b4a027102815606cf72d394d
MOZGIII <mike-n@narod.ru>
2025-01-25 12:27:15 -0300

    Bump deps (#64)
    * Bump prettier and solhint
    
    * Bump deps
    
    * Bump node.js
    
    * Downgrade hardhat-gas-reporter
    
    * Add hardhat ignition
    
    Absolute shit
    
    * Siplify the tests setup
    
    * Update chai

--- tests/hardhat/test/MockBiomapper.ts
+++ tests/hardhat/test/MockBiomapper.ts
@@ -1,4 +1,4 @@
-import { loadFixture } from "@nomicfoundation/hardhat-toolbox-viem/network-helpers";
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
 import hre from "hardhat";
 
