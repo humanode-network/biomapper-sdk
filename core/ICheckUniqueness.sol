ed0ce5ee5b4766c12c1bc304c34d0349680085ed
MOZGIII <mike-n@narod.ru>
2025-01-29 16:19:42 -0300

    Mention that `ICheckUniqueness` is implemented for `BridgedBiomapper`

--- core/ICheckUniqueness.sol
+++ core/ICheckUniqueness.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-/// @dev Interface for the `Biomapper` contract.
+/// @dev Interface for the `Biomapper` and `BridgedBiomapper` contracts.
 /// @notice View the current biomapping state of a given account.
 ///
 /// #### Examples
