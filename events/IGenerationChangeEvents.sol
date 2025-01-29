b7385ac013cac00828d8dc76dc83aa9b822b266b
MOZGIII <mike-n@narod.ru>
2025-01-29 12:27:15 -0300

    Correct the exaustiveness cluase at the events

--- events/IGenerationChangeEvents.sol
+++ events/IGenerationChangeEvents.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 /// @dev Interface for the `Biomapper` contract.
 /// @notice Interface for events related to the biomapper generation changes.
 /// ## Exhaustiveness
-/// This contract may emit other events not covered by this interface.
+/// Contract may emit other events not covered by this interface.
 interface IGenerationChangeEvents {
     /// @notice Emitted when the biomapper generation has been changed.
     event GenerationChanged();
--- events/IProveUniquenessEvents.sol
+++ events/IProveUniquenessEvents.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 /// @dev Interface for the `Biomapper` contract.
 /// @notice Interface for events related to proving uniqueness.
 /// ## Exhaustiveness
-/// This contract may emit other events not covered by this interface.
+/// Contract may emit other events not covered by this interface.
 interface IProveUniquenessEvents {
     /// @notice Emitted when uniqueness is successfully proven and a new biomapping is added.
     /// @param account The address of the account for which uniqueness is proven.
