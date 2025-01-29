fbde1b5277194351a3970c5326d53cc7fcc7fca1
Tony C <tonycdot@yahoo.com>
2025-01-29 20:45:55 +0400

    Update docs

--- core/IBridgedBiomapperRead.sol
+++ core/IBridgedBiomapperRead.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.20;
 /// @title IBridgedBiomapperRead
 /// @notice The interface.
 ///
-/// A bridging tx is a transaction included in a block at the BridgingTxPoint chain that imports data from Humanode chain.
+/// A bridging tx is a transaction included in a block at the bridged chain that imports data from Humanode chain.
 ///
-/// Bridging tx point corresponds to a number of the block at the BridgingTxPoint chain in which a bridging tx is included.
+/// Bridging tx point corresponds to a number of the block at the bridged chain in which a bridging tx is included.
 ///
 /// A bridging tx contains a set of generations and _potentially_ biomappings at Humanode chain.
 /// The generations set can contain up to one generation that is "_being bridged_" and zero or more historical generations.
@@ -23,17 +23,17 @@ interface IBridgedBiomapperRead {
     ///
     /// A new key is populated by the bridging tx for the generation _being bridged_ and
     /// for each historical generations that occurred prior to the generation _being bridged_
-    /// if the corresponding `Generation` is not present at the BridgingTxPoint chain.
+    /// if the corresponding `Generation` is not present at the bridged chain.
     /// For historical generations `generationBridgingTxPointPtr` will be zero.
     ///
     /// #### Fields
     /// | Name | Type | Description |
     /// | ---- | ---- | ----------- |
-    /// | generationBridgingTxPointPtr | uint256 | The block number at the BridgingTxPoint chain when this generation was ported from Humanode chain to this chain. Zero if this generation was bridged as a non-target generation (historical) in the bridging tx. |
+    /// | generationBridgingTxPointPtr | uint256 | The block number at the bridged chain when this generation was ported from Humanode chain to this chain. Zero if this generation was bridged as a non-target generation (historical) in the bridging tx. |
     /// | prevPtr | uint256 | The block number at Humanode chain that points to the previous generation. Zero if this generation is the oldest generation at Humanode chain. |
     /// | nextPtr | uint256 | The block number at Humanode chain that points to the next generation. Zero if this generation is the newest _known_ (bridged) generation at Humanode chain. |
     struct Generation {
-        /// The block number at the BridgingTxPoint chain when this generation was ported from Humanode chain to this chain.
+        /// The block number at the bridged chain when this generation was ported from Humanode chain to this chain.
         /// Zero if this generation was bridged as a non-target generation (historical) in the bridging tx.
         uint256 generationBridgingTxPointPtr;
         /// The block number at Humanode chain that points to the previous generation.
@@ -44,28 +44,28 @@ interface IBridgedBiomapperRead {
         uint256 nextPtr;
     }
 
-    /// @notice Describes a point in time (block number at the BridgingTxPoint chain) when a generation was bridged from Humanode chain.
-    /// @dev Stored in a map where key is a block number at the BridgingTxPoint chain.
+    /// @notice Describes a point in time (block number at the bridged chain) when a generation was bridged from Humanode chain.
+    /// @dev Stored in a map where key is a block number at the bridged chain.
     /// Allows navigating through generation bridging tx points via `prevPtr` and `nextPtr`.
     /// Allows locating `Generation` via `generationPtr`.
     ///
     /// A new key is populated by the bridging tx when it is determined that the
-    /// `Generation` _being bridged_ (i.e. non-historical) is not present at the BridgingTxPoint chain.
+    /// `Generation` _being bridged_ (i.e. non-historical) is not present at the bridged chain.
     ///
     /// #### Fields
     /// | Name | Type | Description |
     /// | ---- | ---- | ----------- |
     /// | generationPtr | uint256 | The block number at Humanode chain corresponding to when the generation was initialized. Never zero. |
-    /// | prevPtr | uint256 | The block number at the BridgingTxPoint chain that points to the previous generation bridging tx point. Zero if this generation bridging tx point is the oldest point at the BridgingTxPoint chain. |
-    /// | nextPtr | uint256 | The block number at the BridgingTxPoint chain that points to the next generation bridging tx point. Zero if this generation bridging tx point is the newest point at the BridgingTxPoint chain. |
+    /// | prevPtr | uint256 | The block number at the bridged chain that points to the previous generation bridging tx point. Zero if this generation bridging tx point is the oldest point at the bridged chain. |
+    /// | nextPtr | uint256 | The block number at the bridged chain that points to the next generation bridging tx point. Zero if this generation bridging tx point is the newest point at the bridged chain. |
     struct GenerationBridgingTxPoint {
         /// The block number at Humanode chain corresponding to when the generation was initialized. Never zero.
         uint256 generationPtr;
-        /// The block number at the BridgingTxPoint chain that points to the previous generation bridging tx point.
-        /// Zero if this generation bridging tx point is the oldest point at the BridgingTxPoint chain.
+        /// The block number at the bridged chain that points to the previous generation bridging tx point.
+        /// Zero if this generation bridging tx point is the oldest point at the bridged chain.
         uint256 prevPtr;
-        /// The block number at the BridgingTxPoint chain that points to the next generation bridging tx point.
-        /// Zero if this generation bridging tx point is the newest point at the BridgingTxPoint chain.
+        /// The block number at the bridged chain that points to the next generation bridging tx point.
+        /// Zero if this generation bridging tx point is the newest point at the bridged chain.
         uint256 nextPtr;
     }
 
@@ -78,19 +78,19 @@ interface IBridgedBiomapperRead {
     /// A new key is populated by the bridging tx for the generation _being bridged_ and
     /// for each historical generations that occurred prior to the generation _being bridged_
     /// if the bridging tx indicates that the account in the bridging tx has the biomapping in a given generation
-    /// and the corresponding `Biomapping` is not present at the BridgingTxPoint chain.
+    /// and the corresponding `Biomapping` is not present at the bridged chain.
     ///
     /// #### Fields
     /// | Name | Type | Description |
     /// | ---- | ---- | ----------- |
     /// | generationPtr | uint256 | The block number at Humanode chain that corresponds to generation in which biomapping has occurred. Never zero. |
-    /// | biomappingBridgingTxPointPtr | uint256 | The block number at the BridgingTxPoint chain when biomapping was ported from Humanode chain to this chain. Zero if this biomapping corresponds to a generation than was bridged as a non-target generation (historical) in the bridging tx. |
+    /// | biomappingBridgingTxPointPtr | uint256 | The block number at the bridged chain when biomapping was ported from Humanode chain to this chain. Zero if this biomapping corresponds to a generation than was bridged as a non-target generation (historical) in the bridging tx. |
     /// | prevPtr | uint256 | The block number at Humanode chain that points to the previous biomapping for the corresponding account. Zero if this biomapping is the oldest biomapping for the corresponding account at Humanode chain. |
     /// | nextPtr | uint256 | The block number at Humanode chain that points to the next biomapping for the corresponding account. Zero if this biomapping is the newest _known_ (bridged) biomapping for the corresponding account at Humanode chain. |
     struct Biomapping {
         /// The block number at Humanode chain that corresponds to generation in which biomapping has occurred. Never zero.
         uint256 generationPtr;
-        /// The block number at the BridgingTxPoint chain when biomapping was ported from Humanode chain to this chain.
+        /// The block number at the bridged chain when biomapping was ported from Humanode chain to this chain.
         /// Zero if this biomapping corresponds to a generation than was bridged as a non-target generation (historical) in the bridging tx.
         uint256 biomappingBridgingTxPointPtr;
         /// The block number at Humanode chain that points to the previous biomapping for the corresponding account.
@@ -101,8 +101,8 @@ interface IBridgedBiomapperRead {
         uint256 nextPtr;
     }
 
-    /// @notice Describes a point in time (block number at the BridgingTxPoint chain) when a biomapping was bridged from Humanode chain.
-    /// @dev Stored in a double map where the keys are an account and a block number at the BridgingTxPoint chain.
+    /// @notice Describes a point in time (block number at the bridged chain) when a biomapping was bridged from Humanode chain.
+    /// @dev Stored in a double map where the keys are an account and a block number at the bridged chain.
     /// Allows navigating through biomapping bridging tx points within a given account via `prevPtr` and `nextPtr`.
     /// Allows locating `GenerationBridgingTxPoint` via `generationBridgingTxPointPtr`.
     /// Allows locating `Biomapping` via `biomappingPtr`.
@@ -112,59 +112,74 @@ interface IBridgedBiomapperRead {
     /// #### Fields
     /// | Name | Type | Description |
     /// | ---- | ---- | ----------- |
-    /// | generationBridgingTxPointPtr | uint256 | The block number at the BridgingTxPoint chain that corresponds to generation bridging tx point when this biomapping was bridged. Never zero. |
+    /// | generationBridgingTxPointPtr | uint256 | The block number at the bridged chain that corresponds to generation bridging tx point when this biomapping was bridged. Never zero. |
     /// | biomappingPtr | uint256 | The block number at Humanode chain corresponding to when the biomap happened. Never zero. |
-    /// | prevPtr | uint256 | The block number at the BridgingTxPoint chain that points to the previous biomapping bridging tx point. Zero if this biomapping bridging tx point is the oldest point at the BridgingTxPoint chain. |
-    /// | nextPtr | uint256 | The block number at the BridgingTxPoint chain that points to the next biomapping bridging tx point. Zero if this biomapping bridging tx point is the newest point at the BridgingTxPoint chain. |
+    /// | prevPtr | uint256 | The block number at the bridged chain that points to the previous biomapping bridging tx point. Zero if this biomapping bridging tx point is the oldest point at the bridged chain. |
+    /// | nextPtr | uint256 | The block number at the bridged chain that points to the next biomapping bridging tx point. Zero if this biomapping bridging tx point is the newest point at the bridged chain. |
     struct BiomappingBridgingTxPoint {
-        /// The block number at the BridgingTxPoint chain that corresponds to generation bridging tx point when this biomapping was bridged.
+        /// The block number at the bridged chain that corresponds to generation bridging tx point when this biomapping was bridged.
         /// Never zero.
         uint256 generationBridgingTxPointPtr;
         /// The block number at Humanode chain corresponding to when the biomap happened. Never zero.
         uint256 biomappingPtr;
-        /// The block number at the BridgingTxPoint chain that points to the previous biomapping bridging tx point.
-        /// Zero if this biomapping bridging tx point is the oldest point at the BridgingTxPoint chain.
+        /// The block number at the bridged chain that points to the previous biomapping bridging tx point.
+        /// Zero if this biomapping bridging tx point is the oldest point at the bridged chain.
         uint256 prevPtr;
-        /// The block number at the BridgingTxPoint chain that points to the next biomapping bridging tx point.
-        /// Zero if this biomapping bridging tx point is the newest point at the BridgingTxPoint chain.
+        /// The block number at the bridged chain that points to the next biomapping bridging tx point.
+        /// Zero if this biomapping bridging tx point is the newest point at the bridged chain.
         uint256 nextPtr;
     }
 
     // `Generation` accessors.
 
-    /// @notice The newest `Generation` ptr.
+    /// @notice Returns the block number at Humanode chain in which the current generation began.
+    /// @return The block number in which the current generation began, 0 if no generations initialized.
     function generationsHead() external view returns (uint256);
 
-    /// @notice The oldest `Generation` ptr.
+    /// @notice Returns the block number at Humanode chain in which the oldest generation began.
+    /// @return The block number in which the oldest generation began, 0 if no generations initialized.
     function generationsTail() external view returns (uint256);
 
-    /// @notice Get `Generation` by ptr.
+    /// @notice Returns the generation struct for a given generation pointer.
+    /// @param ptr The pointer of the requested generation.
+    /// @return The {Generation} structure.
     function generationsListItem(
         uint256 ptr
     ) external view returns (Generation memory);
 
     // `GenerationBridgingTxPoint` accessors.
 
-    /// @notice The newest `GenerationBridgingTxPoint` ptr.
+    /// @notice Returns the block number at bridged chain in which the current generation was bridged.
+    /// @return The block number in which the current generation bridged, 0 if no generations bridged.
     function generationsBridgingTxPointsHead() external view returns (uint256);
 
-    /// @notice The oldest `GenerationBridgingTxPoint` ptr.
+    /// @notice Returns the block number at bridged chain in which the oldest generation was bridged.
+    /// @return The block number in which the oldest generation bridged, 0 if no generations bridged.
     function generationsBridgingTxPointsTail() external view returns (uint256);
 
-    /// @notice Get `GenerationBridgingTxPoint` by ptr.
+    /// @notice Returns the generation bridging tx point struct for a given pointer.
+    /// @param ptr The pointer of the requested bridging tx point.
+    /// @return The {GenerationBridgingTxPoint} structure.
     function generationsBridgingTxPointsListItem(
         uint256 ptr
     ) external view returns (GenerationBridgingTxPoint memory);
 
     // `Biomapping` accessors.
 
-    /// @notice The newest `Biomapping` ptr for a given account.
+    /// @notice Returns the most recent biomapping pointer for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the most recent biomap occurred, or 0 if the account was never bridged.
     function biomappingsHead(address account) external view returns (uint256);
 
-    /// @notice The oldest `Biomapping` ptr for a given account.
+    /// @notice Returns the oldest biomapping pointer for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the oldest biomap occurred, or 0 if the account was never bridged.
     function biomappingsTail(address account) external view returns (uint256);
 
-    /// @notice Get `Biomapping` by ptr for a given account.
+    /// @notice Returns the {Biomapping} struct for a given biomapping pointer and account address.
+    /// @param account The address of the requested account.
+    /// @param ptr The biomapping pointer of the requested biomapping.
+    /// @return The {Biomapping} structure.
     function biomappingsListItem(
         address account,
         uint256 ptr
@@ -172,17 +187,24 @@ interface IBridgedBiomapperRead {
 
     // `BiomappingsBridgingTxPoint` accessors.
 
-    /// @notice The newest `BiomappingsBridgingTxPoint` ptr for a given account.
+    /// @notice Returns the most recent biomapping bridging tx point for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the most recent biomap was bridged, or 0 if the account was never bridged.
     function biomappingsBridgingTxPointsHead(
         address account
     ) external view returns (uint256);
 
-    /// @notice The oldest `BiomappingsBridgingTxPoint` ptr for a given account.
+    /// @notice Returns the oldest biomapping bridging tx point for a given account address.
+    /// @param account The address of the requested account.
+    /// @return The block number in which the oldest biomap was bridged, or 0 if the account was never bridged.
     function biomappingsBridgingTxPointsTail(
         address account
     ) external view returns (uint256);
 
-    /// @notice Get `BiomappingsBridgingTxPoint` by ptr for a given account.
+    /// @notice Returns the {BiomappingBridgingTxPoint} struct for a given pointer and account address.
+    /// @param account The address of the requested account.
+    /// @param ptr The pointer of the requested bridging tx point.
+    /// @return The {BiomappingBridgingTxPoint} structure.
     function biomappingsBridgingTxPointsListItem(
         address account,
         uint256 ptr
@@ -190,17 +212,23 @@ interface IBridgedBiomapperRead {
 
     // Secondary indices.
 
-    /// @notice Get `Biomapping` ptr for a given account in a requested generation.
+    /// @notice Returns the biomapping pointer for a given account address and generation pointer.
     /// @dev Returns non-zero if the account is known to be biomapped in a given generation.
+    /// @param account The address of the requested account.
+    /// @param generationPtr The pointer of the requested generation.
+    /// @return The block number in which the requested user was biomapped in the requested generation, or 0 if there was no biomapping.
     /// Note: can return zero if biomapped at Humanode chain but the information about it is not bridged yet.
     function lookupBiomappingPtr(
         address account,
         uint256 generationPtr
     ) external view returns (uint256);
 
-    /// @notice Get `BiomappingsBridgingTxPoint` ptr for a given account in a requested generation bridged tx point.
+    /// @notice Returns the {BiomappingsBridgingTxPoint} ptr for a given account address and generation bridged tx point.
     /// @dev Returns non-zero if the account biomapping was bridged in a given generation.
-    /// Note: there is a possibility that the biomapping was bridged as part of a bridge tx where the generation it belonged to is a historical generation, in which case the `Biomapping` will exist, but the `BiomappingBridgedTxPoint` will be absent.
+    /// @param account The address of the requested account.
+    /// @param generationBridgedTxPointPtr The pointer of the requested generation.
+    /// @return The block number in which the requested user was biomapped in the requested generation, or 0 if there was no biomapping.
+    /// Note: there is a possibility that the biomapping was bridged as part of a bridge tx where the generation it belonged to is a historical generation, in which case the {Biomapping} will exist, but the {BiomappingBridgedTxPoint} will be absent.
     function lookupBiomappingBridgedTxPointPtr(
         address account,
         uint256 generationBridgedTxPointPtr
